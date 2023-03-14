37a0415d6a005fa2f3a3c50143f361b877975e44
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2023-03-14 08:37:56 +0100

    put proxy list conversion into seperate file

---
+++ text_maker.py
@@ -0,0 +1,14 @@
+from scraping import proxies
+import os.path
+
+proxy_list = [
+    f"{protocol}://{ip}:{port}"
+    for sublist in proxies()
+    for ip, port, protocols in sublist
+    for protocol in protocols
+]
+
+with open(os.path.join('/Users/linushenn/PycharmProjects/ProxyYoink/raw_proxies', 'raw_proxies.txt'), 'w') as f:
+    for proxy in proxy_list:
+        f.write('\n'.join(proxy_list))
+
