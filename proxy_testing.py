e9f6fcf1cdda77352e045b875597af77ed989939
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2023-03-14 14:22:36 +0100

    fix syntax error in proxy_testing

--- proxy_testing.py
+++ proxy_testing.py
@@ -3,7 +3,7 @@ from file_splitting import file_splitting
 
 
 def proxy_testing():
-    for i in range(0-file_splitting()):
+    for i in range(0, file_splitting() + 1):
         if os.path.exists(f'raw_proxies/raw_proxies_{i}.txt'):
             mubeng = os.system(f'mubeng -f raw_proxies/raw_proxies{i}.txt --check --output checked_proxies/checked_proxies.txt')
         else:
