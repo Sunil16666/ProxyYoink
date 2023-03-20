c5a88106352e7bac8af001b1d12db9280f793f50
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2023-03-20 08:47:20 +0100

    fix typo in subprocess run method

--- proxy_testing.py
+++ proxy_testing.py
@@ -1,4 +1,5 @@
 import os.path
+import subprocess
 
 
 def proxy_testing():
@@ -6,10 +7,14 @@ def proxy_testing():
         num_lines = sum(1 for line in open('raw_proxies/raw_proxies.txt'))
         for i in range(1, num_lines // 100000 + 1):
             if os.path.exists(f"raw_proxies/raw_proxies_{i}.txt"):
-                mubeng = os.system(
-                    f"mubeng -f --update raw_proxies/raw_proxies{i}.txt --check --timeout 5 --output checked_proxies/checked_proxies.txt"
+                mubeng = subprocess.run(
+                    f'mubeng -f raw_proxies/raw_proxies_{i}.txt --check --output checked_proxies/checked_proxies.txt --timeout 5s'
                 )
             else:
                 print(f"raw_proxies_{i}.txt not found")
     except FileNotFoundError:
         print('raw_proxies.txt not found')
+
+
+proxy_testing()
+
